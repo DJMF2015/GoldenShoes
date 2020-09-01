0e550c1ef3dab1aa292e33b5c5797ce24bf2c63b
djmf2020 <david@passion4social.com>
2020-09-01 23:06:20 +0100

    initial template for prodct apge rendering and server for express . styles incomplete.made use of ajax request using jquery

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 var express = require('express');
 const app = express();
-
 const port = 8080;
-// app.use(bodyParser.urlencoded({extended: true}));
 
-//set templating engine ejs
 app.set('view engine', 'ejs');
+app.use('/public',express.static('public'));
+
+var arrayDB = require('./public/DBdata.js');
+const items = []
+
+
+console.log(arrayDB);
 
 app.get('/', function(req, res){
-  res.send('hello there')
+  res.send('you are on the HomePage')
+});
+
+app.get('/product/:type', function(req, res){
+  const type = req.params.type;
+  const tempArray = [];
+
+	for(i=0;i<arrayDB.length;i++){
+		if(type === arrayDB[i].type){
+			tempArray.push(arrayDB[i]);
+		}
+	}
+	res.send({products:tempArray});
 });
 
 app.get('/products', function(req, res){
-  res.render('#')
-})
-
-
-
+  res.render('productsPage',{products: arrayDB});
+});
 
 
 
@@ -25,7 +38,7 @@ app.get('/products', function(req, res){
 
 
 app.listen(port, function(){
-   console.log("server is up and running at port " + port);
+  console.log("server is up and running at port " + port);
 });
 
 // module.exports = app;
