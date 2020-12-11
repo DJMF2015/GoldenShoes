adfbee4368439bd3b3cd5770d3805d6e70fa0ff3
djmf2020 <david@passion4social.com>
2020-12-11 18:07:11 +0000

    fix to edit page for contact

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const port = 8080;
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); 
 const expressSanitizer = require('express-sanitizer');
 const ContactRouter = require('./routes/contactRoute.js');
-const ProductRouter = require('./routes/productRoute.js');
- 
+const ProductRouter = require('./routes/productRoute.js');  
+const OrdersRouter = require('./routes/ordersRoute.js');  
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+
+const path = require('path');
 const app = express();
- 
+
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
 app.use(cookieParser());
@@ -17,43 +22,76 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ContactRouter);
 app.use(ProductRouter);
-
-//setup and configure connection to mongodb driver & Mongoose ORM
+app.use(OrdersRouter);
+// const stripe = require('stripe')(stripeSecretKey)
+//setup and establish connection to mongodb client and mongoose orm
 mongoose.connect('mongodb://localhost/goldenshoeDB', { useUnifiedTopology: true, useNewUrlParser: true });
-mongoose.Promise = global.Promise;
 mongoose.connection.once('open', function () {
   console.log('successfully connected to DB...');
 }).on('error', function (err) {
   console.log(err);
 });
- 
- 
+
+
 
 //HOME PAGE
 app.get('/', function (req, res) {
   res.render('home');
 });
 
-// app.get('/index', function(req,res){
-//   res.render('index')
-// });
-
 app.get('/home', function (req, res) {
   res.render('home');
 });
 
+// app.get('/store', function (req, res) {
+//   fs.readFile('items.json', function (error, data) {
+//     if (error) {
+//       res.status(500).end()
+//     } else {
+//       res.render('store.ejs', {
+//         stripePublicKey: stripePublicKey,
+//         items: JSON.parse(data)
+//       })
+//     }
+//   })
+// })
+
+// app.post('/purchase', function (req, res) {
+//   fs.readFile('items.json', function (error, data) {
+//     if (error) {
+//       res.status(500).end()
+//     } else {
+//       const itemsJson = JSON.parse(data)
+//       const itemsArray = itemsJson.music.concat(itemsJson.merch)
+//       let total = 0
+//       req.body.items.forEach(function (item) {
+//         const itemJson = itemsArray.find(function (i) {
+//           return i.id == item.id
+//         })
+//         total = total + itemJson.price * item.quantity
+//       })
+
+//       stripe.charges.create({
+//         amount: total,
+//         source: req.body.stripeTokenId,
+//         currency: 'usd'
+//       }).then(function () {
+//         console.log('Charge Successful')
+//         res.json({ message: 'Successfully purchased items' })
+//       }).catch(function () {
+//         console.log('Charge Fail')
+//         res.status(500).end()
+//       })
+//     }
+//   })
+// })
 // ERROR PAGE
 app.use((req, res) => {
-  res.status(404).send('<h1 style="color:blue; margin-top: 5em;"><center>Sorry, Page not found. Have you checked the correct URL ?<center></h1>');
-});
- 
-// error handling middleware
-app.use(function (err, req, res, next) {
-  console.log(err); // to see properties of message in our console
-  res.status(422).send({ error: err.message });
+  res.status(404).send('<h1>Sorry, Page not found. Have you checked the correct URL ?');
 });
 
-app.listen(port, () => {
-    console.log(`server is up and running at port: ${port}`);
-  });
 
+
+app.listen(port, function () {
+  console.log("server is up and running at port " + port)
+});
--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -1,14 +1,13 @@
 const express = require('express');
-var Contact = require('../models/contact');
+let Contact = require('../models/contact');
 const router = express.Router();
 
+// Add Route
 router.get('/submission', function (req, res) {
-
     Contact.find({}, function (err, contacts) {
         if (!err) {
             res.render("submissionPage", {
                 contacts: contacts
-
             });
         } else {
 
@@ -17,18 +16,13 @@ router.get('/submission', function (req, res) {
 });
 
 
-//compose User contact page
-router.get('/contact', function (req, res) {
-    res.render("pages/contact");
-    
-});
-
+// Add Submit POST Route
 router.post("/contact", function (req, res) {
     var contact = new Contact({
-        name: req.body.postName,
-        email: req.body.postEmail,
-        subject: req.body.postSubject,
-        comment: req.body.postComment
+        name: req.body.name,
+        email: req.body.email,
+        subject: req.body.subject,
+        comment: req.body.comment
     });
 
     //save users contact details
@@ -38,13 +32,77 @@ router.post("/contact", function (req, res) {
         }
     });
 });
-//find one user contact
- 
-// delete a user contact from the db
+
+// Load Edit Form
+router.get('/contact/:id', function (req, res) {
+    Contact.findById(req.params.id, function (err, contact) {
+        if (err) {
+            console.log("not authorised");
+            return res.redirect('/');
+        }
+        res.render('pages/edit', {
+            name: 'Edit Contact Details',
+            contact: contact
+        });
+    });
+});
+
+// Update Submit POST Route
+// router.post('contact/:id', function (req, res ) {
+//     let contact = {};
+//     contact.name = req.body.name;
+//     contact.email = req.body.email;
+//     contact.subject = req.body.subject;
+//     contact.comment = req.body.comment;
+
+//     let query = { _id: req.params.id }
+
+//     Contact.update(query, contact, function (err) {
+//         res.redirect('/submission');
+//     }) ;
+// });
+router.post('/contact/:id', function (req, res, next) {
+    Contact.findByIdAndUpdate(req.params.id, req.body, function (err, response) {
+        if (err) res.json({ message: "Error in updating contact with id " + req.params.id });
+
+        res.redirect('/submission')
+    }) 
+});
+
+// });
+
+// DELETE CONTACT 
 router.get('/contact/delete/:id', function (req, res, next) {
     Contact.findByIdAndRemove({ _id: req.params.id }).then(function () {
-         res.redirect('/submission');
+        res.redirect('/submission');
     }).catch(next);
 });
+
+// Load Contact Form
+router.get('/contact', function (req, res) {
+    res.render("pages/contact");
+
+});
+
+
+// Get Single Contact
+// router.get('/contact/:id', function (req, res) {
+//     Contact.findById(req.params.id), function (req, res) {
+//         res.render('pages/contact', {
+//             contact: contact,
+//             name: name
+//         });
+//     };
+// });
+
+// Add Route
+router.get('/submission', (req, res) => {
+    db.collection('contacts').find().toArray()
+    .then(results =>{
+        console.log(results)
+    })
+    .catch(error => console.error(error))
+})
+
  
 module.exports = router;
