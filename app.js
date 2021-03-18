4dbe4e5e9756f480ef927a99cb12c7d48fac44e4
djmf2020 <david@passion4social.com>
2021-03-18 12:16:33 +0000

    fixed bug to listingPage so add product button works

--- app.js
+++ app.js
@@ -42,20 +42,7 @@ app.get('/', function (req, res) {
 app.get('/home', function (req, res) {
   res.render('home');
 });
-
-// app.get('/store', function (req, res) {
-//   fs.readFile('items.json', function (error, data) {
-//     if (error) {
-//       res.status(500).end()
-//     } else {
-//       res.render('store.ejs', {
-//         stripePublicKey: stripePublicKey,
-//         items: JSON.parse(data)
-//       })
-//     }
-//   })
-// })
-
+ 
 // app.post('/purchase', function (req, res) {
 //   fs.readFile('items.json', function (error, data) {
 //     if (error) {
